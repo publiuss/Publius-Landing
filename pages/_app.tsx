361deb1fa9f27a2fc6367ac44f732fbaf9b0c611
publius <beanstalk.publius@protonmail.com>
2023-01-05 14:38:09 -0500

    script id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,12 +17,12 @@ function MyApp({ Component, pageProps }: EnhancedAppProps) {
     <>
       <Component {...pageProps} />
       {/* Google Analytics */}
-      <Script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <Script id="app-analytics" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-KGLM87W');` }}></Script>
-      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+      <Script id="netlify-id" src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
     </>
   )
 }
